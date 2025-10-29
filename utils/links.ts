0e11141981a27b21c9f60c396ec791d1e44875f2
Martbul <martbul01@gmail.com>
2025-10-29 23:16:49 +0200

    fix: update server uil

--- utils/links.ts
+++ utils/links.ts
@@ -1,9 +1,7 @@
 import { Linking } from "react-native";
 
 export const openTerms = () =>
-  Linking.openURL(
-    "https://outdrinkmeapi.onrender.com/api/v1/terms-of-services"
-  );
+  Linking.openURL("https://martbul.com/api/v1/terms-of-services");
 
 export const openPrivacy = () =>
-  Linking.openURL("https://outdrinkmeapi.onrender.com/api/v1/privacy-policy");
+  Linking.openURL("https://martbul.com/api/v1/privacy-policy");
